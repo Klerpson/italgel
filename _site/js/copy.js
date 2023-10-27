549772406b1ef24fc62e778d799fc6b92ab9ffda
Julian DW <klerpson@gmail.com>
2023-10-27 18:24:14 -0500

    udates

--- _site/js/copy.js
+++
@@ -1,7 +0,0 @@
-function copiarUrl(){
-  let url = document.getElementById('mensaje');
-  let button = document.getElementById('copiar');
-
-  navigator.clipboard.writeText(url.textContent);
-  button.textContent = 'Copiado';
-}
